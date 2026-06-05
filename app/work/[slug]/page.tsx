ef641d2d9d22a69d329c3d39b0850c1814c3a0a3
colinbuzoianu-arch <colinbuzoianu@gmail.com>
2026-06-05 13:16:12 +0300

    Add files via upload

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { PRODUCTS, bySlug } from "../../../lib/products";
 
 export function generateStaticParams() {
   return PRODUCTS
-    .filter((p) => p.slug !== "coupleiq")
+    .filter((p) => p.slug !== "coupleiq" && p.slug !== "buzomed")
     .map((p) => ({ slug: p.slug }));
 }
 
